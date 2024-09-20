21f13457b05764873fce9d626f9349311f3492f3
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-09-20 10:27:19 +0200

    feat: implement LanguageValue to hook language parameter, update URL to accomodate if language is an empty string (all values)

--- src/hooks/useRepository.tsx
+++ src/hooks/useRepository.tsx
@@ -1,14 +1,14 @@
 import useSWR, { Fetcher } from 'swr';
 import { API_URL, TOKEN } from '../constants';
-import { ErrorResponse, RepositorySearchResponse } from '../types';
+import { ErrorResponse, LanguageValue, RepositorySearchResponse } from '../types';
 
 const fetcher: Fetcher<RepositorySearchResponse, string> = (URL: string) =>
   fetch(URL, {
     headers: { 'Content-Type': 'application/vnd.github+json', Authorization: `Bearer ${TOKEN}` },
   }).then((res) => res.json());
 
-export function useRepository(language: string) {
-  const URL = `${API_URL}/repositories?q=language:${language}&per_page=1`;
+export function useRepository(language: LanguageValue) {
+  const URL = `${API_URL}/repositories?q=${language ? `language:${language}&` : ''}per_page=100`;
   const { data, error, isLoading } = useSWR<RepositorySearchResponse, ErrorResponse>(URL, fetcher);
 
   return { data, error, isLoading };
