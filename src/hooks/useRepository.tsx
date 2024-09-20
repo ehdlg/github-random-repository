6af7cdb1458087eb6f16a8cbfaa5fe58adbbf473
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-09-20 12:08:22 +0200

    feat(hooks): implement useCache hook to get data from cache if available before fetching data

--- src/hooks/useRepository.tsx
+++ src/hooks/useRepository.tsx
@@ -1,6 +1,7 @@
 import useSWR, { Fetcher } from 'swr';
 import { API_URL, TOKEN, REPOSITORIES_PER_PAGE } from '../constants';
 import { ErrorResponse, LanguageValue, RepositorySearchResponse } from '../types';
+import useCache from './useCache';
 
 const fetcher: Fetcher<RepositorySearchResponse, string> = (URL: string) =>
   fetch(URL, {
@@ -11,9 +12,16 @@ export function useRepository(language: LanguageValue) {
   const URL = `${API_URL}/repositories?q=${
     language ? `language:${language}&` : ''
   }per_page=${REPOSITORIES_PER_PAGE}`;
-  const { data, error, isLoading } = useSWR<RepositorySearchResponse, ErrorResponse>(URL, fetcher);
+  const { cachedRepos, updateCache, isCacheLoading } = useCache(URL);
+  const fetcherURL = null == cachedRepos && !isCacheLoading ? URL : null;
+  const { data, error, isLoading } = useSWR<RepositorySearchResponse, ErrorResponse>(
+    fetcherURL,
+    fetcher
+  );
 
-  return { data, error, isLoading };
+  if (null != data) updateCache(URL, data.items);
+
+  return { data: cachedRepos || data?.items, error, isLoading: isCacheLoading || isLoading };
 }
 
 export default useRepository;
