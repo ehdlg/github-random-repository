cb0b6a11cbfe50498cff777d87d485a7f8f3bae3
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-09-20 06:33:40 +0200

    feat: implement types to swr objects

--- src/hooks/useRepository.tsx
+++ src/hooks/useRepository.tsx
@@ -1,13 +1,15 @@
-import useSWR from 'swr';
+import useSWR, { Fetcher } from 'swr';
 import { API_URL } from '../constants';
+import { ErrorResponse, RepositorySearchResponse } from '../types';
 
-const fetcher = (URL: string) => fetch(URL).then((res) => res.json());
+const fetcher: Fetcher<RepositorySearchResponse, string> = (URL: string) =>
+  fetch(URL).then((res) => res.json());
 
 export function useRepository(language: string) {
   const URL = `${API_URL}/repositories?q=language:${language}&per_page=1`;
-  const { data: repository, error, isLoading } = useSWR(URL, fetcher);
+  const { data, error, isLoading } = useSWR<RepositorySearchResponse, ErrorResponse>(URL, fetcher);
 
-  return { repository, error, isLoading };
+  return { data, error, isLoading };
 }
 
 export default useRepository;
