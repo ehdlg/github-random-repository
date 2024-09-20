656bcd3286c4a4ba0ca315592bec2d44e650cc95
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-09-20 09:26:03 +0200

    feat(hooks): add github api token to request headers

--- src/hooks/useRepository.tsx
+++ src/hooks/useRepository.tsx
@@ -1,9 +1,11 @@
 import useSWR, { Fetcher } from 'swr';
-import { API_URL } from '../constants';
+import { API_URL, TOKEN } from '../constants';
 import { ErrorResponse, RepositorySearchResponse } from '../types';
 
 const fetcher: Fetcher<RepositorySearchResponse, string> = (URL: string) =>
-  fetch(URL).then((res) => res.json());
+  fetch(URL, {
+    headers: { 'Content-Type': 'application/vnd.github+json', Authorization: `Bearer ${TOKEN}` },
+  }).then((res) => res.json());
 
 export function useRepository(language: string) {
   const URL = `${API_URL}/repositories?q=language:${language}&per_page=1`;
