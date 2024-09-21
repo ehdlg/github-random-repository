7c8e9df19be833427c34f43488c949f819c83e16
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-09-21 17:22:05 +0200

    feat(components): add Loading component

---
+++ src/Loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className='flex mt-20 justify-center min-h-screen'>
+      <div className='w-16 h-16 border-8  border-violet-500  border-b-transparent rounded-full animate-spin'></div>
+    </div>
+  );
+}
