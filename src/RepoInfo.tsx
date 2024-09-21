b72b05f8ff342e2cc08c71681e1f5664db891ebe
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-09-21 22:16:40 +0200

    feat: add button to update the random repository

--- src/RepoInfo.tsx
+++ src/RepoInfo.tsx
@@ -3,37 +3,52 @@ import { IoMdStar } from 'react-icons/io';
 import { RiGitForkFill } from 'react-icons/ri';
 import { PiWarningCircleLight } from 'react-icons/pi';
 
-export default function RepoInfo({ repo }: { repo: Repository }) {
+export default function RepoInfo({
+  repo,
+  refreshRepo,
+}: {
+  repo: Repository;
+  refreshRepo: () => void;
+}) {
   const { description, language, watchers_count, forks_count, open_issues_count, name, html_url } =
     repo;
   const iconStyles = 'text-violet-500 size-5';
   const spanStyles = 'flex gap-2 items-center';
 
   return (
-    <article className='w-full bg-slate-200 dark:bg-slate-800 rounded-xl  text-slate-800 dark:text-slate-200 p-8 flex flex-col gap-8 text-left text-lg'>
-      <a href={html_url} target='_blank'>
-        <h3 className='text-3xl text-left from-slate-200 to-slate-200 hover:cursor-pointer bg-gradient-to-br hover:from-violet-300 hover:to-violet-400 inline-block text-transparent bg-clip-text transition ease-in duration-200'>
-          {name}
-        </h3>
-      </a>
-      <p className='dark:text-slate-400 text-slate-700 text-2xl'>
-        {description ?? 'No description provided '}
-      </p>
-      <div className='text-left flex w-full text-lg justify-between text-slate-300 items-center'>
-        <span className={spanStyles}>
-          <div className={`size-3 rounded-full bg-violet-500 language-${language}`}></div>{' '}
-          {language}
-        </span>
-        <span className={spanStyles}>
-          <IoMdStar className={iconStyles} /> {watchers_count}
-        </span>
-        <span className={spanStyles}>
-          <RiGitForkFill className={iconStyles} /> {forks_count}
-        </span>
-        <span className={spanStyles}>
-          <PiWarningCircleLight className={iconStyles} /> {open_issues_count}
-        </span>
-      </div>
-    </article>
+    <>
+      <article className='w-full bg-slate-200  min-h-[350px] justify-between dark:bg-slate-800 rounded-xl  text-slate-800 dark:text-slate-200 p-8 flex flex-col gap-8 text-left text-lg'>
+        <a href={html_url} target='_blank'>
+          <h3 className='text-3xl text-left from-slate-200 to-slate-200 hover:cursor-pointer bg-gradient-to-br hover:from-violet-300 hover:to-violet-400 inline-block text-transparent bg-clip-text transition ease-in duration-200'>
+            {name}
+          </h3>
+        </a>
+        <p className='dark:text-slate-400 text-slate-700 text-2xl mb-auto'>
+          {description ?? 'No description provided '}
+        </p>
+        <div className='text-left flex w-full text-lg justify-between  text-slate-300 items-center'>
+          <span className={spanStyles}>
+            <div className={`size-3 rounded-full bg-violet-500 language-${language}`}></div>{' '}
+            {language}
+          </span>
+          <span className={spanStyles}>
+            <IoMdStar className={iconStyles} /> {watchers_count}
+          </span>
+          <span className={spanStyles}>
+            <RiGitForkFill className={iconStyles} /> {forks_count}
+          </span>
+          <span className={spanStyles}>
+            <PiWarningCircleLight className={iconStyles} /> {open_issues_count}
+          </span>
+        </div>
+      </article>
+
+      <button
+        onClick={refreshRepo}
+        className='w-full dark:bg-slate-800 px-2 py-4 border-2 border-transparent hover:border-violet-500 rounded-xl text-xl drop-shadow-xl hover:drop-shadow-none transition ease-in duration-250 dark:text-slate-200 text-slate-800'
+      >
+        Refresh
+      </button>
+    </>
   );
 }
