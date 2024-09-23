97d7164bad57ed526c2424cb8d4f2295b8b06957
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-09-23 06:38:23 +0200

    refactor: optimize cache usage by caching only essential repository properties

--- src/hooks/useRepository.ts
+++ src/hooks/useRepository.ts
@@ -1,7 +1,8 @@
 import useSWR, { Fetcher } from 'swr';
 import { API_URL, TOKEN, REPOSITORIES_PER_PAGE } from '../constants';
-import { ErrorResponse, LanguageValue, RepositorySearchResponse } from '../types';
+import { ErrorResponse, LanguageValue, Repository, RepositorySearchResponse } from '../types';
 import useCache from './useCache';
+import { extractRequiredFields } from '../utils';
 
 const fetcher: Fetcher<RepositorySearchResponse, string> = (URL: string) =>
   fetch(URL, {
@@ -9,6 +10,7 @@ const fetcher: Fetcher<RepositorySearchResponse, string> = (URL: string) =>
   }).then((res) => res.json());
 
 export function useRepository(language: LanguageValue) {
+  let filteredRepos: Repository[] | null = null;
   const URL = `${API_URL}/repositories?q=${
     language ? `language:${language}&` : ''
   }per_page=${REPOSITORIES_PER_PAGE}`;
@@ -19,9 +21,13 @@ export function useRepository(language: LanguageValue) {
     fetcher
   );
 
-  if (null != language && null != data) updateCache(language, data.items);
+  if (null != language && null != data) {
+    filteredRepos = extractRequiredFields(data.items);
 
-  return { data: cachedRepos || data?.items, error, isLoading: isCacheLoading || isLoading };
+    updateCache(language, filteredRepos);
+  }
+
+  return { data: cachedRepos || filteredRepos, error, isLoading: isCacheLoading || isLoading };
 }
 
 export default useRepository;
