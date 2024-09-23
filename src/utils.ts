14a00122d7106f4da33d40b2a7bd575fff4421ab
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-09-23 06:25:05 +0200

    feat(utils): add extractRequiredFields function

--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,36 @@
-import { FullRepository } from './types';
+import { FullRepository, Repository } from './types';
 
 export const getRandomRepo = (repositories: FullRepository[]) => {
   const repositoriesLength = repositories.length;
   const randomIndex = Math.floor(Math.random() * repositoriesLength);
 
   return repositories[randomIndex];
+};
+
+export const extractRequiredFields = (fullRepositories: FullRepository[]): Repository[] => {
+  const repositories: Repository[] = fullRepositories.map((fullRepo) => {
+    const {
+      description,
+      forks_count,
+      html_url,
+      id,
+      language,
+      name,
+      open_issues_count,
+      stargazers_count,
+    }: Repository = fullRepo;
+
+    return {
+      description,
+      forks_count,
+      html_url,
+      id,
+      language,
+      name,
+      open_issues_count,
+      stargazers_count,
+    };
+  });
+
+  return repositories;
 };
