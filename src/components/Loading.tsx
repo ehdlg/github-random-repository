7a9f1d97bd2c50b3cac7536ee84ae9a4d1d0c202
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-09-23 06:17:30 +0200

    feat(styles): decrase border size, increase size

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading() {
   return (
     <div className='flex mt-20 justify-center min-h-screen'>
-      <div className='w-16 h-16 border-8  border-violet-500  border-b-transparent rounded-full animate-spin'></div>
+      <div className='w-24 h-24 border-4  border-violet-500  border-b-transparent rounded-full animate-spin'></div>
     </div>
   );
 }
