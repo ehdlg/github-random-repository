f4b6b4f4b51be50a8cfa27a894ea60720a7a5ee8
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-09-23 06:50:10 +0200

    build: add sonner

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './components/App.tsx';
+import { Toaster } from 'sonner';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Toaster richColors position='top-right' closeButton />
     <App />
   </StrictMode>
 );
