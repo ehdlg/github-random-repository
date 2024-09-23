a92429cbf9e5d1acc05603de2ada131263b57bff
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-09-23 10:48:49 +0200

    feat(validation): add middleware to validate request body repositories

--- backend/src/middlewares.ts
+++ backend/src/middlewares.ts
@@ -1,4 +1,5 @@
 import { ErrorRequestHandler, RequestHandler } from 'express';
+import { CustomError } from './errors';
 
 export const notFound: RequestHandler = (req, res, next) => {
   res.status(404).json('Not found');
@@ -9,4 +10,13 @@ export const handleError: ErrorRequestHandler = (error, req, res, next) => {
   const status = error.status ?? 500;
 
   return res.status(status).json(message);
+};
+
+export const validateRepositoriesInput = (repos: string) => {
+  try {
+    return JSON.parse(repos);
+  } catch {
+    const error = new CustomError('Invalid repositories', 404);
+    throw error;
+  }
 };
