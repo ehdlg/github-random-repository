bc8771ad283f3a7290f0dbc8f6c6c3b290825412
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-09-25 09:37:45 +0200

    feat(db): add object config to redis client

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -1,6 +1,14 @@
 import { createClient } from 'redis';
 
-export const client = createClient();
+const { REDIS_PWD, REDIS_HOST, REDIS_PORT } = process.env;
+
+export const client = createClient({
+  password: REDIS_PWD || undefined,
+  socket: {
+    host: REDIS_HOST || 'localhost',
+    port: Number(REDIS_PORT) || 6379,
+  },
+});
 
 export const checkRedisConnection = async () => {
   try {
